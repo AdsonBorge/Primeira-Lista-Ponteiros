92038fead6d3b82e6ad2460e99ffc130bcedf06c
AdsonBorge <78302919+AdsonBorge@users.noreply.github.com>
2021-02-13 22:28:35 -0300

    Add files via upload

---
+++ Questão08.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+
+Considerando a declaração:
+
+int mat[4], *p, x;
+
+quais das seguintes expressões são válidas? Justifique.
+
+p = mat + 1;
+Inválida, o endereço 'p' não pode receber um valor inteiro. 
+
+p = mat;
+Inválida, endereço 'p' não pode receber valor inteiro.
+
+x = (*mat);
+Válida, permite que x receba o primeiro valor do vetor mat.
+
+*/
+
+//Compilação de código para correção, não foi conseguido resposta satisfatória.
+
+int main(){
+
+int mat[4]= {1, 2, 3, 4};
+int *p, x;
+
+p = mat + 1;
+p = mat;
+p = mat;
+x = (*mat);
+
+    return 0;
+}
